f973d2f05f4d51c9131e6ab28d70c4c7c5a32651
Bryan Hilldrup <hilldrupbf@vcu.edu>
2020-05-02 22:53:20 -0400

    Added manager card skeleton

--- app/views/cinemas/index.php
+++ app/views/cinemas/index.php
@@ -129,109 +129,136 @@
                 </div>
             </div>
         </div>
-        <div class="col card mx-2 border-dark">
-            <p class="card-header text-center border-dark"><strong>Finances - <span id="day2">Daily</span><span id="month2" style="display: none;">Monthly</span></strong></p>
-            <form class="text-center card-header mt-1 mb-4 py-1 border-dark" action="<?php echo URLROOT; ?>/cinemas/<?php echo $data['cinema_id'] ?>" method="post">
-                <label for="datepicker">Select a date:</label>
-                <input type="date" value="<?php echo $data['finances']['date_chosen']; ?>" name="date_chosen" id="datepicker">
-                <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-search"></i></button>
-            </form>
-            <div class="card-container">
 
+        <!-- Middle Column -->
+        <div class="col">
+            <div class="card-columns d-flex flex-column justify-content-center">
 
-                <div id="day">
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Tickets Sold:</span>
-                        <span class="text-right"><?php echo $data['finances']['total_tickets']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Gross Sales:</span>
-                        <span class="text-right">$<?php echo $data['finances']['gross_sales']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Credit Card Sales:</span>
-                        <span class="text-right">$<?php echo $data['finances']['CREDIT']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Cash Sales:</span>
-                        <span class="text-right">$<?php echo $data['finances']['CASH']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Gift Card Sales:</span>
-                        <span class="text-right">$<?php echo $data['finances']['GIFT']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Number of Transactions: </span>
-                        <span class="text-right"><?php echo $data['finances']['transactions']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-                </div>
-
-                <div id="month" style="display: none;">
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Tickets Sold:</span>
-                        <span class="text-right"><?php echo $data['monthly_finances']['total_tickets']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Gross Sales:</span>
-                        <span class="text-right">$<?php echo $data['monthly_finances']['gross_sales']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Credit Card Sales:</span>
-                        <span class="text-right">$<?php echo $data['monthly_finances']['CREDIT']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Cash Sales:</span>
-                        <span class="text-right">$<?php echo $data['monthly_finances']['CASH']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Gift Card Sales:</span>
-                        <span class="text-right">$<?php echo $data['monthly_finances']['GIFT']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-
-
-                    <div class="d-flex justify-content-between">
-                        <span class="text-left font-weight-bold">Number of Transactions: </span>
-                        <span class="text-right"><?php echo $data['monthly_finances']['transactions']; ?></span>
-                    </div>
-                    <div class="border-bottom w-100 mb-3"></div>
-                </div>
-
-
-            </div>
-            <div class="mx-2">
-                <div class="card-footer border-dark">
-                    <div class="btn-group btn-group-toggle d-flex justify-content-center" data-toggle="buttons">
-                        <label class="btn btn-primary active">
-                            <input type="radio" style="box-shadow: none;" onchange="hideMonthly(this)" name="option1" id="daily" autocomplete="off" checked> Daily
-                        </label>
-                        <label class="btn btn-primary">
-                            <input type="radio" onchange="hideDaily(this)" name="option2" id="monthly" autocomplete="off"> Monthly
-                        </label>
+                <!-- Manager Card -->
+                <div class="col card border-dark">
+                    <div class="card-container">
+                        <p class="card-header mb-2 text-center border-dark"><strong>Manager</strong></p>
+                        <div class="manager-container d-flex justify-content-center">
+                            <div class="rounded-circle overflow-hidden mx-2 custom-circle-image">
+                                <img class="w-100 h-100" src="<?php echo URLROOT; ?>/public/img/profile-icon.png" alt="Card image cap">
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 class="ml-4 mt-3 mb-1 mt-4">Bryan Hilldrup</h5>
+                                <p class="text-center"><small class="font-weight-bold ml-4">Manager ID: 300</small></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+                <!-- Finances Card -->
+                <div class="col card mb-0 border-dark">
+                    <p class="card-header text-center border-dark"><strong>Finances - <span id="day2">Daily</span><span id="month2" style="display: none;">Monthly</span></strong></p>
+                    <form class="text-center card-header mt-1 mb-4 py-1 border-dark" action="<?php echo URLROOT; ?>/cinemas/<?php echo $data['cinema_id'] ?>" method="post">
+                        <label for="datepicker">Select a date:</label>
+                        <input type="date" value="<?php echo $data['finances']['date_chosen']; ?>" name="date_chosen" id="datepicker">
+                        <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                    <div class="card-container">
+
+
+                        <div id="day">
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Tickets Sold:</span>
+                                <span class="text-right"><?php echo $data['finances']['total_tickets']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Gross Sales:</span>
+                                <span class="text-right">$<?php echo $data['finances']['gross_sales']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Credit Card Sales:</span>
+                                <span class="text-right">$<?php echo $data['finances']['CREDIT']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Cash Sales:</span>
+                                <span class="text-right">$<?php echo $data['finances']['CASH']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Gift Card Sales:</span>
+                                <span class="text-right">$<?php echo $data['finances']['GIFT']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Number of Transactions: </span>
+                                <span class="text-right"><?php echo $data['finances']['transactions']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+                        </div>
+
+                        <div id="month" style="display: none;">
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Tickets Sold:</span>
+                                <span class="text-right"><?php echo $data['monthly_finances']['total_tickets']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Gross Sales:</span>
+                                <span class="text-right">$<?php echo $data['monthly_finances']['gross_sales']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Credit Card Sales:</span>
+                                <span class="text-right">$<?php echo $data['monthly_finances']['CREDIT']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Cash Sales:</span>
+                                <span class="text-right">$<?php echo $data['monthly_finances']['CASH']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Gift Card Sales:</span>
+                                <span class="text-right">$<?php echo $data['monthly_finances']['GIFT']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+
+
+                            <div class="d-flex justify-content-between">
+                                <span class="text-left font-weight-bold">Number of Transactions: </span>
+                                <span class="text-right"><?php echo $data['monthly_finances']['transactions']; ?></span>
+                            </div>
+                            <div class="border-bottom w-100 mb-3"></div>
+                        </div>
+
+
+                    </div>
+                    <div class="mx-2">
+                        <div class="card-footer border-dark">
+                            <div class="btn-group btn-group-toggle d-flex justify-content-center" data-toggle="buttons">
+                                <label class="btn btn-primary active">
+                                    <input type="radio" style="box-shadow: none;" onchange="hideMonthly(this)" name="option1" id="daily" autocomplete="off" checked> Daily
+                                </label>
+                                <label class="btn btn-primary">
+                                    <input type="radio" onchange="hideDaily(this)" name="option2" id="monthly" autocomplete="off"> Monthly
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
+
+        <!-- Shows Card -->
         <div class="col card mx-2 border-dark">
             <p class="card-header text-center border-dark"><strong>Shows</strong></p>
             <div class="card-body">
@@ -254,5 +281,24 @@
                 <a href="<?php echo URLROOT; ?>/cinemas/modify_movie" class="btn btn-success w-100">Add New Movie</a>
             </div>
         </div>
+        <!-- <div class="col">
+  <div class="card-columns d-flex flex-column justify-content-center">
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a longer card with shorter text.</p>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+
+
     </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
